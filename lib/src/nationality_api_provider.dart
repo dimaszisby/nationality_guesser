0bc8f67ad17660a69aa9970a5e6a576243e72908
dimaszisby <dimaszisby@gmail.com>
2022-07-26 20:18:50 +0700

    Add: Nationality API Provider

--- lib/src/nationality_api_provider.dart
+++ lib/src/nationality_api_provider.dart
@@ -5,6 +5,21 @@ import './models/person_model.dart';
 
 class NationalityApiProvider {
   Client client = Client();
-  final _apiKey = '';
+  final _apiKey = ''; //No API key for this API
+
+  Future<Person> fetchResult() async {
+    print('Fetching Person on API');
+
+    final response = await client.get("https://api.nationalize.io?name=michael");
+    print(response.body.toString());
+
+    if (response.statusCode == 200) {
+      //successfull -> parse JSON
+      return Person.fromJson(json.decode(response.body));
+    } else {
+      //not sucessful -> throw error
+      throw Exception('Failed to Fetch Data');
+    }
+  }
 
 }
