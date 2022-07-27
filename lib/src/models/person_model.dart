0700bbabd239f50cfb59995c3e6400f11a058db6
dimaszisby <dimaszisby@gmail.com>
2022-07-27 09:13:43 +0700

    Update: person_model factory and mapper function for JSON serialization

--- lib/src/models/person_model.dart
+++ lib/src/models/person_model.dart
@@ -2,6 +2,10 @@ import 'package:flutter/foundation.dart';
 import 'dart:async';
 import 'dart:convert';
 
+//NOTE: Experiments on Data Model when error
+
+String newsModelToJson(Person data) => json.encode(data.toJson());
+
 class Person {
   final String name;
   final List<Country> countries;
@@ -11,6 +15,8 @@ class Person {
     required this.countries,
   });
 
+  //version 1 -> cant run on the UI StreamBuilder because of Data Type
+  /*
   factory Person.fromJson(Map<String, dynamic> parsedJson) {
     final name = parsedJson['name'] as String;
     final countriesData = parsedJson['country'] as List<dynamic>;
@@ -23,6 +29,19 @@ class Person {
       countries: countries,
     );
   }
+*/
+
+  //version 2
+  factory Person.fromJson(Map<String, dynamic> json) => Person(
+        name: json["name"],
+        countries:
+            List<Country>.from(json["country"].map((x) => Country.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "country": List<dynamic>.from(countries.map((x) => x.toJson())),
+      };
 }
 
 class Country {
@@ -34,6 +53,8 @@ class Country {
     required this.probability,
   });
 
+  //Version 1 --> cant be used on Stream Builder
+  /*
   factory Country.fromJson(Map<String, dynamic> parsedJson) {
     final country_id = parsedJson['country_id'] as String;
     final probability = parsedJson['probability'] as double;
@@ -44,6 +65,18 @@ class Country {
   Map<String, dynamic> toJson() {
     return {'country_id': country_id, 'probability': probability};
   }
+*/
+
+  //version 2
+  factory Country.fromJson(Map<String, dynamic> json) => Country(
+        country_id: json["country_id"],
+        probability: json["probability"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "country_id": country_id,
+        "probability": probability,
+      };
 }
 
 //TODO: Delete this when are not needed
