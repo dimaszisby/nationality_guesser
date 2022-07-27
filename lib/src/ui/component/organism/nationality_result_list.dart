8640c570f51710df99c7826299fe5b7eff9b0c85
dimaszisby <dimaszisby@gmail.com>
2022-07-27 10:20:30 +0700

    Update: /nationality_result_list.dart --> updating the UI and business logic implementation (NOT FINISHED)

--- lib/src/ui/component/organism/nationality_result_list.dart
+++ lib/src/ui/component/organism/nationality_result_list.dart
@@ -1,44 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:nationality_guesser_app/src/blocs/person_bloc.dart';
 import 'package:nationality_guesser_app/src/models/person_model.dart';
 
 class NationalityResultList extends StatelessWidget {
   // const MyWidget({Key? key}) : super(key: key);
 
-  final String countryResult;
-  final double probabilityResult;
+  // final String countryResult;
+  // final double probabilityResult;
 
-  NationalityResultList(this.countryResult, this.probabilityResult);
+  final _personBloc = GuesserBloc();
+  late String name;
+
+  // NationalityResultList(this.countryResult, this.probabilityResult);
 
   List<DummyPerson> dummyPerson = [
     DummyPerson('Dimas', 'Indonesia', 1.00),
   ];
 
   @override
-  Widget build(BuildContext context) {
-    final resultCard = Card(
-      child: Row(
-        children: const [
-          Text(
-            'Dimas',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            '1.00',
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          )
-        ],
-      ),
-    );
+  void initState() {
+    _personBloc.eventSink.add(GuesserAction.Fetch);
+    // super.initState();
+  }
 
-    return Row(
-      children: <Widget>[
-        Card(),
-      ],
+  @override
+  void dispose() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Name Here'),
+      ),
+      body: Container(
+        child: StreamBuilder<List<Country>>(
+          stream: _personBloc.nationalitiesStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index) {
+                  var country = snapshot.data?[index];
+                  return Container(
+                    child: Flexible(
+                      child: Column(children: [
+                        Text(country!.country_id),
+                        Text('${country.probability}'),
+                      ]),
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
