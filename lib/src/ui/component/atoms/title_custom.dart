d229826af5ffc7c5d86b3a79a912e931dce4c941
dimaszisby <dimaszisby@gmail.com>
2022-07-29 09:31:10 +0700

    Update: Styling /search_name_.dart /title_cusom.dart

--- lib/src/ui/component/atoms/title_custom.dart
+++ lib/src/ui/component/atoms/title_custom.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
 
 class TitleCustom extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(left: 10),
-      child: Text(
-        'Nationality Predictor',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 48,
-          fontWeight: FontWeight.bold,
-        ),
+    final mediaQuery = MediaQuery.of(context);
+    return SizedBox(
+      child: Column(
+        children: [
+          SizedBox(
+            child: Row(
+              children: [
+                  Container(
+                  color: Colors.white,
+                  width: mediaQuery.size.width * 0.2,
+                  height: mediaQuery.size.width * 0.1,
+                ),
+                const SizedBox(width: 10,),
+                const Text(
+                  'Nationality',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                color: Colors.white,
+                width: mediaQuery.size.width * 0.1,
+                height: mediaQuery.size.width * 0.1,
+              ),
+              const SizedBox(width: 5,),
+              const SizedBox(
+                child: Text(
+                  'Guesser',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/src/ui/component/molecules/search_name_card.dart
+++ lib/src/ui/component/molecules/search_name_card.dart
@@ -29,84 +29,89 @@ class _SearchNameCardState extends State<SearchNameCard> {
     final mediaQuery = MediaQuery.of(context);
 
     return Container(
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: const EdgeInsets.only(right: 10,),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            right: 10,
+          ),
+          child: Container(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+            decoration: const BoxDecoration(
+              boxShadow: [BoxShadow(blurRadius: 10)],
+            ),
             child: Container(
-              padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom + 10),
-              decoration: const BoxDecoration(
-                boxShadow: [BoxShadow(blurRadius: 10)],
-              ),
-              child: Container(
-                height: mediaQuery.size.width * 0.8,
-                width: mediaQuery.size.width * 8,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.transparent,
-                  ),
-                  borderRadius: const BorderRadius.only(
-                    topRight: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
-                  ),
+              height: mediaQuery.size.width * 0.8,
+              width: mediaQuery.size.width * 8,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  color: Colors.transparent,
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Flexible(
-                        child: TextField(
-                          controller: _txController,
-                          autofocus: true,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Name",
-                            focusColor: Colors.teal, 
-                          ),
-                          onChanged: (text) {
-                            setState(
-                              () {
-                                personName = text;
-                              },
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Flexible(
+                      child: TextField(
+                        controller: _txController,
+                        autofocus: true,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Name",
+                          focusColor: Colors.teal,
+                        ),
+                        onChanged: (text) {
+                          setState(
+                            () {
+                              personName = text;
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Flexible(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_txController.text.isNotEmpty) {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => NationalityResultList(
+                                  submittedName: _txController.text,
+                                ),
+                              ),
                             );
-                          },
-                        ),
+                          } else {
+                            return;
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.teal,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 50, vertical: 15),
+                            textStyle: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold)),
+                        child: const Text('Search'),
                       ),
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      Flexible(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            if (_txController != null) {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => NationalityResultList(
-                                        submittedName: _txController.text,
-                                      )));
-                            } else {
-                              return;
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                              primary: Colors.teal,
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 50, vertical: 15),
-                              textStyle: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold)),
-                          child: const Text('Search'),
-                        ),
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
               ),
             ),
           ),
         ),
-      );
-    
+      ),
+    );
   }
 }
