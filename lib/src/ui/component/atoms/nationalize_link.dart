75267acdb82081f2f2d9416b497ff9ab35cebe26
dimaszisby <dimaszisby@gmail.com>
2022-08-03 08:52:29 +0700

    Update: Hyperlink Creation -->Adding permision on AndroidManifest.xml (Android) and info.plist (IOS)

--- lib/src/ui/component/atoms/nationalize_link.dart
+++ lib/src/ui/component/atoms/nationalize_link.dart
@@ -1,32 +1,40 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
 
 class NationalizeLink extends StatelessWidget {
   const NationalizeLink({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: const [
-        Text(
-          "Learn more about",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w300,
-          ),
-        ),
-        SizedBox(
-          width: 5,
-        ),
-        Text(
-          "nationalize.io",
-          style: TextStyle(
-            color: Colors.amber,
-            fontWeight: FontWeight.bold,
-            decoration: TextDecoration.underline,
-          ),
-        ),
-      ],
+    return Center(
+      child: Link(
+        uri: Uri.parse("https://nationalize.io/"),
+        builder: (context, followLink) {
+          return RichText(
+            text: TextSpan(
+              children: [
+                const TextSpan(
+                  text: 'Learn more about ',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                TextSpan(
+                  text: 'nationalize.io',
+                  style: const TextStyle(
+                    color: Colors.amber,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                  ),
+                  recognizer: TapGestureRecognizer()..onTap = followLink,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
